0e8f0437c1c556fc809c748b3cd18ec2e919c0b7
Ernesto Zeferino Diaz <ernesto.zefe.d@gmail.com>
2021-02-23 17:19:44 -0600

    Lectura de excel

--- src/main/scala/com/zefe/searchnamings/App.scala
+++ src/main/scala/com/zefe/searchnamings/App.scala
@@ -16,19 +16,36 @@ object App {
   def main(args : Array[String]): Unit = {
     val res = new Resource()
 
-    //procesingExcel(res)
+    val schemaSource = scala.io.Source.fromFile(res.pathSchemaAll).getLines.mkString
+    val schemaFromJson = DataType.fromJson(schemaSource).asInstanceOf[StructType]
+
+    val df = res.spark.read
+      .format("com.crealytics.spark.excel")
+      .option("sheetName", "DDNG-N") // Required
+      .option("header", "true") // Required
+      .option("treatEmptyValuesAsNulls", "false") // Optional, default: true
+      .option("inferSchema", "false") // Optional, default: false
+      .option("addColorColumns", "true") // Optional, default: false
+      .option("startColumn", 0) // Optional, default: 0
+      .option("endColumn", 56) // Optional, default: Int.MaxValue
+      .option("maxRowsInMemory", 20) // Optional, default None. If set, uses a streaming reader which can help with big files
+      .option("excerptSize", 10)
+      .schema(schemaFromJson)
+      .load("src/main/resources/DDNG-N-8634.xlsx")
+
+    df.show()
+    println(df.count())
+
+    df.write.partitionBy(
+      "mexico_mark_of_use"
+    ).mode(
+      SaveMode.Overwrite
+    ).parquet(res.pathNamings2)
 
 
 
-    val searchNaming = new SearchNaming(res)
-      .words("fecha")
-      .words("alta")
-    .search.orderBy(
-      col("mexico_mark_of_use").desc,
-      col("type_naming").asc,
-      length(col("logical_name_of_the_field_spa")).asc,
-      col("global_naming_field").asc
-    ).show(100,false)
+
+
 
 
   }
